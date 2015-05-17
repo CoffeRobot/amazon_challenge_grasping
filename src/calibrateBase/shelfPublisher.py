ccf960821a460161d0406e19454a7ddccbdce8e6
Francisco Vina <fevb@kth.se>
2015-05-17 14:37:05 +0200

    removed system call for shelf publisher priority

--- src/calibrateBase/shelfPublisher.py
+++ src/calibrateBase/shelfPublisher.py
@@ -1,11 +1,9 @@
 #! /usr/bin/python
 
 from baseScan import *
-import os
 
 
 rospy.init_node('shelf_publisher', anonymous=True)
 # rospy.sleep(15.0)
-os.nice(1000)
 bs = baseScan()
 bs.publish2TF()
